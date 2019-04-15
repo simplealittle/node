ea9468b1d89bc45dd96f8ac3829bf643b828e296
耿沅标 <gengyb@chanjet.com>
2019-04-15 11:42:37 +0800

    no message

--- controllers/get.js
+++ controllers/get.js
@@ -1,6 +1,6 @@
 var get=async (ctx,next)=>{
 	var params=ctx.query||{};
-	console.log("11111111144441")
+	console.log("11111111133331")
 	ctx.response.body=params.name+""+params.psw;
 }
 module.exports={
